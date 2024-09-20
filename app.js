d4993c9bbb19a0b7834cd8bdf0c943a04be06678
Crib1212 <igwepatrickchinedu@gmail.com>
2024-09-20 16:18:51 +0100

    20th september

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ closeCart.addEventListener('click', () => {
                 newProduct.innerHTML = 
                 `<img src="${product.image}" alt="">
                 <h2>${product.name}</h2>
-                <div class="price">$${product.price}</div>
+                <div class="price">&#8358;${product.price}</div>
                 <button class="addCart">Add To Cart</button>`;
                 listProductHTML.appendChild(newProduct);
             });
